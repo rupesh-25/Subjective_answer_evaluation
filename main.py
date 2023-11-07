f6b2e767e2ae96097acf2aa926b2f67e05a8fc1e
Rupesh <suryawanshirupesh25@gmail.com>
2023-11-07 10:14:06 +0530

    Nothing

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ app=Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def home():
-    return render_template('base.html')
+    return render_template('home.html')
 
 @app.route('/submit',methods=['GET','POST'])
 def submit():
+    #I would add Langchain code here
+    #For now it would just return 5
     return '<h1>5</h1>'
 
 if __name__=='__main__':
