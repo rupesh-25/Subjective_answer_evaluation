7f5dbde5895a789cd11aa6709f9be0fffb1c5bb6
Rupesh <suryawanshirupesh25@gmail.com>
2023-11-08 19:28:49 +0530

    Basic Front end done
    Basic frontend is completed. For completion of base project, only backend code is remaining

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ app=Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def home():
-    return render_template('home.html')
+    return render_template('base.html')
 
 @app.route('/submit',methods=['GET','POST'])
 def submit():
     #I would add Langchain code here
     #For now it would just return 5
-    return '<h1>5</h1>'
+    scores=80
+    return render_template('home.html',score=scores)
 
 if __name__=='__main__':
     app.run(debug=True)
